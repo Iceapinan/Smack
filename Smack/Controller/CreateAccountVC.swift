de172ffa565454c3572e209ca6bd80ca4b12510b
Iceapinan <ice.ap2543@gmail.com>
2017-08-12 22:53:51 +0700

    Add RoundedButton to Login and CreateAccount views

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+        
+    }
+    @IBAction func pickBackgroundColorPressed(_ sender: Any) {
+        
+        
     }
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTextField.text, emailTextField.text != "" else { return }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else { return }
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+
+    }
     
 
 }
